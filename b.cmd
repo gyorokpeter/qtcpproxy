63806127a5d7a7dfa1f8f671a9be4b0ac63281f7
gyorokpeter <gyorokpeter@freemail.hu>
2020-09-13 22:18:28 +0100

    add check for c.dll in current directory

--- b.cmd
+++ b.cmd
@@ -1,3 +1,7 @@
 cls
 @call config.cmd
-g++ -o qtcpproxy qtcpproxy.cpp -I%KX_KDB_PATH%/c/c %KX_KDB_PATH%/w32/c.dll -lws2_32
+@if not exist c.dll (
+	echo copy c.dll from %KX_KDB_PATH%/w32/c.dll to .
+	exit 1
+)
+g++ -o qtcpproxy qtcpproxy.cpp -I%KX_KDB_PATH%/c/c  c.dll -lws2_32
